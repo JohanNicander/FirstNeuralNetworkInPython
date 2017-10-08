0b8b7e37fa2892bae7098a74cb8943faf148882c
Unknown <joelkarlsson97@gmail.com>
2017-10-08 18:46:18 +0200

    NN start
    Början på en __init__ till NN

--- Program Files/main.py
+++ Program Files/main.py
@@ -16,12 +16,27 @@ def johan():
 
 
 class NeuralNet:
-    def __init__(self, strucvec=np.array([1, 1])):
-        self.strucvec = strucvec
+    def __init__(self, neuralshape=np.array([1, 1])):
+        self.neuralshape = neuralshape
+        self.a = []
+        self.W = []
+        j = 0
+        for i in neuralshape:
+            self.a.append(np.zeros(i))
+            self.W.append(np.zeros(np.array([i, j])))
+            j = i
 
 
 def joel():
     matr = np.array([[0, 1, 2, 3],
                      [0, 1, 2, 4]])
     plt.plot(matr[0, :], matr[1, :])
-    plt.show()
+    # plt.show()
+    neuralnettest = NeuralNet(np.array([2, 4, 3]))
+    print(str(neuralnettest.a))
+    print(str(neuralnettest.W))
+    print(str(np.dot(neuralnettest.W[1], neuralnettest.a[0])))
+    print(str(neuralnettest.W[1][2, 1]))
+
+
+joel()
