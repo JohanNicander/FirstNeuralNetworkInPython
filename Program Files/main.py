c98d71e5f1db74ccff763da2e75117f9597508bf
Johan Nicander <mr.johannicander@gmail.com>
2017-10-08 18:58:04 +0200

    FIxade johan

--- Program Files/main.py
+++ Program Files/main.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 import numpy as np
+import os
 
 
 def johan():
     # Read data
-    indata = open('Data.txt', 'r')
+    path = os.path.dirname(os.path.abspath(__file__))
+    indata = open(path + r'/Data.txt')
     dataframe = pd.read_fwf(indata)
     x_values = dataframe[['X']]
     y_values = dataframe[['Y']]
+    print(x_values)
+
+    # Liear regression
+    reg = linear_model.LinearRegression()
+    reg.fit(x_values, y_values)
 
     # Visulize results
     plt.scatter(x_values, y_values)
+    plt.plot(x_values, reg.predict(x_values))
     plt.show()
+
+
+johan()
 
 
 class NeuralNet:
