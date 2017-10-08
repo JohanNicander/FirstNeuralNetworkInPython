0911b926d95cbd20e1d921ad47240c1820a4f1e1
Unknown <joelkarlsson97@gmail.com>
2017-10-08 18:14:34 +0200

    Rensat main.py
    Snyggat till filen

--- Program Files/main.py
+++ Program Files/main.py
@@ -1,81 +1,29 @@
-<<<<<<< HEAD
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
-
-
-# Read data
-indata = open('Data.txt', 'r')
-dataframe = pd.read_fwf(indata)
-x_values = dataframe[['X']]
-y_values = dataframe[['Y']]
-
-# Visulize results
-plt.scatter(x_values, y_values)
-plt.show()
-=======
 import numpy as np
-import matplotlib.pyplot as plt
->>>>>>> master
 
 
-def function(var1, var2):
-    print(var1)
+def johan():
+    # Read data
+    indata = open('Data.txt', 'r')
+    dataframe = pd.read_fwf(indata)
+    x_values = dataframe[['X']]
+    y_values = dataframe[['Y']]
 
-
-class NeuralNet:        # hej
-    def __init__(self, strucvec=np.array([1, 1])):
-        self.strucvec = strucvec
-
-
-<<<<<<< HEAD
-def plot():
-    matr = np.array([[0, 1, 2, 3],
-                     [0, 1, 2, 4]])
-    pplot.plot(matr[0, :], matr[1, :])
-    # pplot.show()
-    print(1)
-
-
-plot()
-=======
-matr = np.array([[0, 1, 2, 3],
-                 [0, 1, 2, 4]])
-plt.plot(matr[0, :], matr[1, :])
-plt.show()
->>>>>>> master
-
-
-def function(var1, var2):
-    print(var1)
-
-
-class NeuralNet:        # hej
-    def __init__(self, strucvec=np.array([1, 1])):
-        self.strucvec = strucvec
-
-
-def plot():
-    matr = np.array([[0, 1, 2, 3],
-                     [0, 1, 2, 4]])
-    plt.plot(matr[0, range(4)], matr[1, range(4)])
+    # Visulize results
+    plt.scatter(x_values, y_values)
     plt.show()
-    print(1)
+
+# JOEL
 
 
-plot()
-
-
-def function(var1, var2):
-    print(var1)
-
-
-class NeuralNet:        # hej
+class NeuralNet:
     def __init__(self, strucvec=np.array([1, 1])):
         self.strucvec = strucvec
 
 
-matr = np.array([[0, 1, 2, 3],
-                 [0, 1, 2, 4]])
-plt.plot(matr[0, :], matr[1, :])
-plt.show()
+def joel():
+    matr = np.array([[0, 1, 2, 3],
+                     [0, 1, 2, 4]])
+    plt.plot(matr[0, :], matr[1, :])
+    plt.show()
