2490bef6b6a663b77b70f313e2ece0b752e00aaf
Johan Nicander <mr.johannicander@gmail.com>
2017-10-09 23:37:54 +0200

    Mest tänkt och komenterat
    Vi behöver väll inte ha a.
    Hur loopar man över listor med np.ndarry-objekt? (gissar att index är bra)
    la till några exeptions

--- Program Files/main.py
+++ Program Files/main.py
@@ -1,15 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import linear_model
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn import linear_model
 import numpy as np
-import os
+# import os
 # import abc    används för att göra abstract base class (typ interface)
 
 
-def sigmoid(x):
-    if type(x) is not np.array:
-        raise TypeError("Wrong input type to sigmoid")
-    return np.divide(1, np.add(1, np.exp(-x)))
+def tempfun(x):
+    return x
 
 
 class NeuralNet:
@@ -20,49 +18,53 @@ class NeuralNet:
     # self.b,       list of vectors containing biases
     # self.actfun,  aktiveringsfunktion
 
-    def __init__(self, neuralShape=np.array([1, 1]), actfun=sigmoid):
+    def __init__(self, neuralShape=np.array([1, 1]), actfun=tempfun):
         self.setNeuralShape(neuralShape, actfun)
 
+    # Vill ha =self.neuralShape, =self.actfun, som default
     def setNeuralShape(self, neuralShape=None, actfun=None):
-        # Input checks
         if neuralShape is None:
             pass
+            # Här gör vi nått smart
         elif type(neuralShape) is np.ndarray and neuralShape.ndim == 1:
             self.neuralShape = neuralShape
         else:
-            # TODO: Indikera att något gått fel
-            pass
-
-        if actfun is None:
-            pass
-        elif callable(actfun):
+            raise ValueError("argument neuralShape must be a numpy array")
+        if callable(actfun):
             self.actfun = actfun
         else:
-            # TODO: Indikera att något gått fel
-            pass
-
-        # Initialize a, W, b
+            raise ValueError("argument actfun must be a vector function")
         self.a = []
         self.W = []
         self.b = []
         j = 0
         for i in self.neuralShape:
             self.a.append(np.zeros(i))
+            # TODO: Vi inte explicit definera a.
             self.W.append(np.random.random_sample(np.array([i, j])))
             self.b.append(np.random.random(i))
             j = i
         self.W.pop(0)
         self.b.pop(0)
 
+    def propagate(self, input):
+        # fattar inte hur man lopar över en lista med np.ndarryobjekt
+        # förutsätter att vi inte definerar a
+        '''
+        for w in self.W and b in self.b     <---bara för att få fram tanken
+            input=actfun(np.dot(w, input)+b)
+        return input
+        '''
+
 
 def johan():
-    # Read data
+    '''# Read data
     path = os.path.dirname(os.path.abspath(__file__))
     indata = open(path + r'/Data.txt')
     dataframe = pd.read_fwf(indata)
     x_values = dataframe[['X']]
     y_values = dataframe[['Y']]
-    print(x_values)
+    # print(x_values)
 
     # Liear regression
     reg = linear_model.LinearRegression()
@@ -73,17 +75,25 @@ def johan():
     plt.plot(x_values, reg.predict(x_values))
     plt.show()
 
+    mylist = [np.ndarray([[1, 2, 3],
+                          [1, 3, 5]]), np.ndarray([4, 5, 6, 7]),
+              np.ndarray([8, 9, 10])]
+    mylist = [1, 2, 3]
+    len(mylist)
+    # for x in range(mylist):
+    #    print(x)'''
+
 
 def joel():
     matr = np.array([[0, 1, 2, 3],
                      [0, 1, 2, 4]])
     try:
-        temp = np.add(1, matr)
+        temp = np.add(matr, matr)
     except:
         print("Doesn't work")
     else:
         print(str(temp))
-    plt.plot(matr[0, :], matr[1, :])
+    # plt.plot(matr[0, :], matr[1, :])
     # plt.show()
     neuralnettest = NeuralNet(np.array([2, 4, 3]))
     try:
@@ -99,6 +109,3 @@ def joel():
     else:
         var_exists = True
     print(var_exists)
-
-
-joel()
