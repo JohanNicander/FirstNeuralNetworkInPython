ab002c0cb573cd22c4d5c8363ed29927c1e7725f
Johan Nicander <mr.johannicander@gmail.com>
2017-12-11 22:16:16 +0100

    Save load mer

--- Program Files/neuralNetGo.py
+++ Program Files/neuralNetGo.py
@@ -1,4 +1,5 @@
 import neuralNet as nn
+import neuralFuns as nf
 import numpy as np
 
 
@@ -6,16 +7,18 @@ def fun(x):
     return x.prod(axis=0).reshape(1, x.shape[1])
 
 
-shape = np.array([2, 5, 5, 5, 1])
-net = nn.NeuralNet(shape)
-x = 20 * np.random.random_sample([10000, 2])
-print(x)
+shape = np.array([2, 5, 5, 1])
+actfun = [[nf.sigmoid, nf.sigmoidPrime]]
+actfun.append([nf.linear, lambda x: np.ones(x.shape)])
+net = nn.NeuralNet(shape,)
+x = np.array(np.random.random_integers(1, 10, [10000, 2]))
 x = x.T
+print(x)
 y = fun(x)
 y.shape = [1, y.size]
 
 print(net.cost(x, y))
-res = net.optimCost(x, y, setx=True, method='BFGS', options={'maxiter': 10000})
+res = net.optimCost(x, y, method='Newton-CG', options={'maxiter': 10000})
 print(net.getState())
 print(net.cost(x, y))
 
@@ -27,3 +30,10 @@ temp = [np.array([[10], [7]]), np.array([[53], [17]]), np.array([[29], [45]]),
         np.array([[95], [72]])]
 for x in temp:
     print(net.error(x, fun(x)))
+
+x = np.random.random_integers(1, 10, [5, 2]).T
+print(x)
+y = fun(x)
+y.shape = [1, y.size]
+print(np.mean(net.error(x, y)))
+print(net.propagate(x))
